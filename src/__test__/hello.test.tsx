b7857945c9bbb6340661db1fbf6d4f32e64e9881
Quang Huy <huy.tran@gotecq.com>
2024-09-04 16:35:57 +0700

    chore: update report

--- src/__test__/hello.test.tsx
+++ src/__test__/hello.test.tsx
@@ -20,6 +20,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(2, 2)).toBe(3)
-// })
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(2, 2)).toBe(3)
+})
