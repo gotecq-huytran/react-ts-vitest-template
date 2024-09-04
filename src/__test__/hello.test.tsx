00d62538c4bccc273c6ff434bc3e1c1df09490f2
Quang Huy <huy.tran@gotecq.com>
2024-09-04 15:05:05 +0700

    chore: adding failure case

--- src/__test__/hello.test.tsx
+++ src/__test__/hello.test.tsx
@@ -19,3 +19,7 @@ test("renders welcome message", () => {
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(2, 2)).toBe(3)
+})
