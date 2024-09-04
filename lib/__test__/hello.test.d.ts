a9ab26aee26c0a348453015fd8adef3f310bd6e8
Quang Huy <huy.tran@gotecq.com>
2024-09-04 10:27:53 +0700

    feat: add ttypescript

---
+++ lib/__test__/hello.test.d.ts
@@ -0,0 +1,1 @@
+export declare function sum(a: any, b: any): any;
--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
         reporters: ['default', 'junit'],
         outputFile: './report.xml',
         setupFiles: "./lib/__test__/setup.js",
+        alias: {
+            '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/fileMock.js',
+            '\\.(css|less|scss)$': '<rootDir>/config/styleMock.js',
+        },
         // you might want to disable it, if you don't have tests that rely on CSS
         // since parsing CSS is slow
         css: false,
