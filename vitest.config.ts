d80b88f4d6d4fddc8231be58c86322743ec9f84e
Quang Huy <huy.tran@gotecq.com>
2024-09-04 15:17:43 +0700

    update: config

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,10 +9,11 @@ export default defineConfig({
         environment: 'jsdom',
         root: '.',
         dir: './lib/__test__/',
-        reporters: ['default', 'junit'],
+        reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
         outputFile: {
             junit: './report.xml'
         },
+
         setupFiles: "./lib/__test__/setup.js",
         alias: {
             '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/fileMock.js',
