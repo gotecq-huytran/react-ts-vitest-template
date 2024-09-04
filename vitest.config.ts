31fda339325707d8f62ddba26d4355d700add385
Quang Huy <huy.tran@gotecq.com>
2024-09-04 17:27:07 +0700

    feat: transform xml file

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,9 +10,9 @@ export default defineConfig({
         root: '.',
         dir: './lib/__test__/',
         reporters: ['default', 'junit'],
-        // outputFile: {
-        //     junit: './report.xml'
-        // },
+        outputFile: {
+            junit: './report.xml'
+        },
         setupFiles: "./lib/__test__/setup.js",
         alias: {
             '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/fileMock.js',
